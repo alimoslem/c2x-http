533ca8cc27161b13717c21ecfa86309531274b4e
M Amin Nasiri <khodexenon@gmail.com>
2021-08-14 17:59:28 +0430

    add generate_zombie_token func

--- modules/get_modules.py
+++ modules/get_modules.py
@@ -5,6 +5,8 @@ this script contains modules to get data from files and return desired values
 '''
 
 import json
+from random import randint
+from hashlib import md5
 
 
 def get_config_value(main_key):
@@ -21,3 +23,17 @@ def get_config_value(main_key):
         print('Error finding : ' + main_key + ' in config.json')
     else:
         return value
+
+def generate_zombie_token():
+    '''
+    create a random token for zombies
+    '''
+    random_number1 = str(randint(1, 1000000))
+    random_number2 = str(randint(1, 1000000))
+    random_number3 = str(randint(1, 1000000))
+
+    combined = f'{random_number1}{random_number2}{random_number3}'
+    my_hash = md5()
+    my_hash.update(combined.encode())
+    token = str(my_hash.hexdigest())
+    return token
